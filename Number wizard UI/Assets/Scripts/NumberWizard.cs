fafabeefd904b893efbd9150c4b56f070e256e20
Shrey Aggarwal <shreyagg2202@users.noreply.github.com>
2021-01-20 22:48:46 +0530

    Initial commit

---
+++ Number wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startgame();  
+    }
+
+    void startgame()
+    {
+        NextGuess();
+    }
+
+    // Update is called once per frame
+    public void OnPressHigher()
+    {
+        if (min < max)
+            min = guess + 1;
+
+        else
+            min = guess;
+
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
