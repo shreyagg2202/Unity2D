bb5a0641facac04c1284277af13702b935c27700
Shrey Aggarwal <shreyagg2202@users.noreply.github.com>
2021-01-21 00:06:51 +0530

    initial commit

---
+++ Reflectica/text101/Assets/AdventureGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = ("Wake up! Wake up!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
