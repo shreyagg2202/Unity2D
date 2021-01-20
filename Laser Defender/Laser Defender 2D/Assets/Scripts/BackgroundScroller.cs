d90c2320722998270c6629c5640c65a6ac3bb093
Shrey Aggarwal <shreyagg2202@users.noreply.github.com>
2021-01-21 02:44:53 +0530

    Initial commit

---
+++ Laser Defender/Laser Defender 2D/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.5f;
+    Material myMaterial;
+    Vector2 offset;
+
+    public void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offset = new Vector2(0f, backgroundScrollSpeed);
+    }
+
+    public void Update()
+    {
+        myMaterial.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
+
---
+++ Laser Defender/Laser Defender 2D/Assets/Scripts/Level.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] float DelayInSeconds = 2f;
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+        FindObjectOfType<GameSession>().Resetgame();
+    }
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(DelayQuitGameScene());
+    }
+
+    IEnumerator DelayQuitGameScene()
+    {
+        yield return new WaitForSeconds(DelayInSeconds);
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
