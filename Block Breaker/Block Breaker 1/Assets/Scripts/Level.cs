c327e41ac6a6be46524a18b834e3dc6eac2e1843
Shrey Aggarwal <shreyagg2202@users.noreply.github.com>
2021-01-21 01:46:56 +0530

    Initial commit

---
+++ Block Breaker/Block Breaker 1/Assets/Scripts/Level.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    // Parameters
+    [SerializeField] int breakableBlocks;              // Serialized for debugging purposes
+
+    // cached reference
+    SceneLoader sceneLoader;
+
+    public void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <=0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+    
+}
---
+++ Pong Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Ball")]
+    public GameObject ball;
+
+    [Header("Player1")]
+    public GameObject player1Paddle;
+    public GameObject player1Goal;
+
+
+    [Header("Player2")]
+    public GameObject player2Paddle;
+    public GameObject player2Goal;
+
+    [Header("Score UI")]
+    public GameObject Player1Text;
+    public GameObject Player2Text;
+
+    private int Player1Score;
+    private int Player2Score;
+
+    public void Player1Scored()
+    {
+        Player1Score++;
+        Player1Text.GetComponent<TextMeshProUGUI>().text = Player1Score.ToString();
+        ResetPosition();
+    }
+
+    public void Player2Scored()
+    {
+        Player2Score++;
+        Player2Text.GetComponent<TextMeshProUGUI>().text = Player2Score.ToString();
+        ResetPosition();
+    }
+
+    public void ResetPosition()
+    {
+        ball.GetComponent<Ball>().Reset();
+        player1Paddle.GetComponent<Paddle>().Reset();
+        player2Paddle.GetComponent<Paddle>().Reset();
+    }
+}
