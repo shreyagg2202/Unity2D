a8b83f1fb8a5f1ce3f0342c561aa8cd8b3d772ea
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-03-16 01:38:42 +0530

    Added SFX

--- TileVania/Assets/Scripts/CoinPickup.cs
+++ TileVania/Assets/Scripts/CoinPickup.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickupSFX;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
