5a21db1ed6db27884bd68de0f782802e173af89e
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-03-16 01:53:19 +0530

    Added score

--- TileVania/Assets/Scripts/CoinPickup.cs
+++ TileVania/Assets/Scripts/CoinPickup.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickupSFX;
+    [SerializeField] int pointsForCoinPickup = 100;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickup);
         AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
--- TileVania/Assets/Scripts/GameSession.cs
+++ TileVania/Assets/Scripts/GameSession.cs
@@ -3,10 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int playerLives = 3;
+    [SerializeField] int score = 0;
+
+    [SerializeField] Text livesText;
+    [SerializeField] Text scoreText;
+
 
     private void Awake()
     {
@@ -24,7 +29,14 @@ public class GameSession : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        livesText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
+    }
+
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = score.ToString();
     }
 
    public void ProcessPlayerDeath()
@@ -40,16 +52,17 @@ public class GameSession : MonoBehaviour
 
     }
 
-    private void ResetGameSession()
-    {
-        SceneManager.LoadScene(0);
-        Destroy(gameObject);
-    }
-
     private void TakeLife()
     {
         playerLives--;
         var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+        livesText.text = playerLives.ToString();
+    }
+
+    private void ResetGameSession()
+    {
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
     }
 }
