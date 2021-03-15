dbc8f6ad493ede2386aaaf643d4f6f623f531d6f
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-03-15 23:39:51 +0530

    Added Main Menu and Win Screen

---
+++ TileVania/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] float levelExitSlowMoFactor = 0.2f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = levelExitSlowMoFactor;
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        Time.timeScale = 1f;
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+}
