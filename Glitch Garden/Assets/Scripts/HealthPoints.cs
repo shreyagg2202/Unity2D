a0854c76873c91fc129a0b29f5b7dbbdab471d74
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-03-20 23:28:44 +0530

    Debugging

--- Glitch Garden/Assets/Scripts/HealthPoints.cs
+++ Glitch Garden/Assets/Scripts/HealthPoints.cs
@@ -13,7 +13,6 @@ public class HealthPoints : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(this);
         lives = baseLives - PlayerPrefsController.GetDifficulty();
         livesText = GetComponent<Text>();
         UpdateDisplay();
