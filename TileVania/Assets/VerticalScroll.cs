d56cb2c93724400fca44fa33d0cf1595b849e952
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-03-20 15:21:13 +0530

    debugging

---
+++ TileVania/Assets/VerticalScroll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalScroll : MonoBehaviour
+{
+    [Tooltip ("Game units per second")]
+    [SerializeField] float scrollRate = 0.2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yMove = scrollRate * Time.deltaTime;
+        transform.Translate(new Vector2(0f, yMove));
+    }
+}
