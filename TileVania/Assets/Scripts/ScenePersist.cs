9870ca16f29a7e1d4ec51940a04c7764bf4292c3
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-03-20 15:34:00 +0530

    Finalizing Game

--- TileVania/Assets/Scripts/ScenePersist.cs
+++ TileVania/Assets/Scripts/ScenePersist.cs
@@ -4,32 +4,27 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ScenePersist : MonoBehaviour
 {
-    int startingSceneIndex;
+    int sceneIndex;
     private void Awake()
     {
-        int numScenePersist = FindObjectsOfType<ScenePersist>().Length;
-        if (numScenePersist > 1) 
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            DontDestroyOnLoad(gameObject);
-        }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        startingSceneIndex = SceneManager.GetActiveScene().buildIndex;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        if (currentSceneIndex != startingSceneIndex)
+        ScenePersist[] persists = FindObjectsOfType<ScenePersist>();
+        foreach (var persist in persists)
         {
-            Destroy(gameObject);
+            if (persist != this)
+            {
+                if (persist.sceneIndex == currentSceneIndex)
+                {
+                    Destroy(gameObject);
+                    return;
+                }
+                else
+                {
+                    Destroy(persist.gameObject);
+                }
+            }
         }
+        sceneIndex = currentSceneIndex;
+        DontDestroyOnLoad(gameObject);
     }
 }
