63f29ab61458034c58ac994e33ab9fe9f3418b80
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-03-22 17:02:37 +0530

    Added Pacman Movement

---
+++ Pacman/Assets/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float enemySpeed = 5f;
+
+    Rigidbody2D myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
