6a66f4e149973726721bce62948b27bb651f0e9e
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-03-22 20:40:09 +0530

    Added Functionality to hold the last player direction

---
+++ Pacman/Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float enemySpeed = 5f;
+    [SerializeField] float waitTime;
+    float startWaitTime;
+    [SerializeField] Transform[] moveSpots;
+    int randomSpot;
+
+    Rigidbody2D myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, enemySpeed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
--- Pacman/Assets/Scripts/Pacman.cs
+++ Pacman/Assets/Scripts/Pacman.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Pacman : MonoBehaviour
 {
     [SerializeField] float pacmanSpeed = 1f;
+    bool isFacingRight;
+    bool isFacingUp;
 
     Rigidbody2D myRigidBody;
     Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,22 +23,61 @@ public class Pacman : MonoBehaviour
     {
         PacmanXPosMove();
         PacmanYPosMove();
+        LastMovement();
+       
     }
 
-    private void PacmanXPosMove()
+    private void PacmanXPosMove()                               //X moving direction of character
     {
         float xControlThrow = Input.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(xControlThrow * pacmanSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
 
-        animator.SetFloat("Horizontal", xControlThrow);
+        animator.SetFloat("SpeedX", xControlThrow);
     }
 
-    private void PacmanYPosMove()
+    private void PacmanYPosMove()                               //Y moving direction of character
     {
         float yControlThrow = Input.GetAxis("Vertical");
         Vector2 playerVelocity = new Vector2(myRigidBody.velocity.x, yControlThrow * pacmanSpeed);
         myRigidBody.velocity = playerVelocity;
-        animator.SetFloat("Vertical", yControlThrow);
+
+        animator.SetFloat("SpeedY", yControlThrow);
+    }
+
+    private void LastMovement()                                   //Holds the last moving directioni of the character
+    {
+        float lastInputX = Input.GetAxis("Horizontal");
+        float lastInputY = Input.GetAxis("Vertical");
+
+        if (lastInputX != 0 || lastInputY != 0)
+        {
+            if (lastInputX > 0)
+            {
+                animator.SetFloat("LastMoveX", 1f);
+            }
+            else if (lastInputX < 0)
+            {
+                animator.SetFloat("LastMoveX", -1f);
+            }
+            else
+            {
+                animator.SetFloat("LastMoveX", 0f);
+            }
+
+            if (lastInputY > 0)
+            {
+                animator.SetFloat("LastMoveY", 1f);
+            }
+            else if (lastInputY < 0)
+            {
+                animator.SetFloat("LastMoveY", -1f);
+            }
+            else
+            {
+                animator.SetFloat("LastMoveY", 0f);
+            }
+        }
+
     }
 }
