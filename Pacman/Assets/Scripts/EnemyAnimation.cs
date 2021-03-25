f0f30584de364d74d2420ed22646d93f043f3cc3
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-03-26 03:49:41 +0530

    Working on enemy Animation

---
+++ Pacman/Assets/Scripts/EnemyAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    Animator myBodyAnimation;
+    float currentPosX;
+    float currentPosY;
+    float newPosX;
+    float newPosY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBodyAnimation = GetComponent<Animator>();
+        currentPosX = transform.position.x;
+        currentPosY = transform.position.y;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPosX = transform.position.x;
+        newPosY = transform.position.y;
+    }
+
+    public void MovingUp()
+    {
+        if ()
+    }
+}
