6204c93e4558173752e03ddf3af34f523a5a8b3f
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-04-16 19:53:16 +0530

    Portal Completed

---
+++ Pacman/Assets/AstarPathfindingProject/Behaviors/LeftPortal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftPortal : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] Transform rightPortal;
+    public bool teleporting = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && teleporting == false)
+        {
+            StartCoroutine(Teleport());
+        }
+    }
+
+    IEnumerator Teleport()
+    {
+        teleporting = true;
+        player.transform.position = rightPortal.transform.position;
+        yield return new WaitForSeconds(1);
+        teleporting = false;
+    }
+}
---
+++ Pacman/Assets/AstarPathfindingProject/Behaviors/RightPortal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightPortal : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] Transform leftPortal;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && FindObjectOfType<LeftPortal>().teleporting == false)
+        {
+            StartCoroutine(Teleport());
+        }
+    }
+
+    IEnumerator Teleport()
+    {
+        FindObjectOfType<LeftPortal>().teleporting = true;
+        player.transform.position = leftPortal.transform.position;
+        yield return new WaitForSeconds(1);
+        FindObjectOfType<LeftPortal>().teleporting = false;
+    }
+}
