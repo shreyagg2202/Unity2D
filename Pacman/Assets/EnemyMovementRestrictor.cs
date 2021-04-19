d39f756f7773a52701af6e68673e413744f07f43
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-04-19 14:59:06 +0530

    Improving Enemy Movement

--- Pacman/Assets/EnemyMovementRestrictor.cs
+++ Pacman/Assets/EnemyMovementRestrictor.cs
@@ -5,61 +5,66 @@ using UnityEngine;
 public class EnemyMovementRestrictor : MonoBehaviour
 {
     bool faceUp, faceDown, faceLeft, faceRight;
-    Animator parentBodyAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
-        parentBodyAnimator = GetComponentInParent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(faceUp);
-        var rotationVector = transform.rotation.eulerAngles;
         EnemyFacing();
         if (faceUp == true)
         {
-            transform.position = new Vector3(0, -1);
-            rotationVector.z = 90;
+            transform.localPosition = new Vector3(0, -1);
         }
         else if (faceDown == true)
         {
-            transform.position = new Vector3(0, 1);
-            rotationVector.z = 90;
+            transform.localPosition = new Vector3(0, 1);
         }
         else if (faceLeft == true)
         {
-            transform.position = new Vector3(1, 0);
-            rotationVector.z = 0;
+            transform.localPosition = new Vector3(1, 0);
         }
         else if (faceRight == true)
         {
-            transform.position = new Vector3(-1, 0);
-            rotationVector.z = 0;
+            transform.localPosition = new Vector3(-1, 0);
         }
     }
 
     private void EnemyFacing()
     {
         //Get animation from blend tree with the help pf the float values managing it
-        Debug.Log("hello");
         if (FindObjectOfType<EnemyAnimation>().yMov >= 0)
         {
-            Debug.Log("Byee");
             faceUp = true;
+            faceDown = false;
+            faceLeft = false;
+            faceRight = false;
 
         }
         else if (FindObjectOfType<EnemyAnimation>().yMov <= 0)
         {
+            faceUp = false;
             faceDown = true;
+            faceLeft = false;
+            faceRight = false;
         }
         else if (FindObjectOfType<EnemyAnimation>().xMov >= 0)
         {
+            faceUp = false;
+            faceDown = false;
             faceLeft = true;
+            faceRight = false;
         }
         else if (FindObjectOfType<EnemyAnimation>().xMov <= 0)
         {
+            faceUp = false;
+            faceDown = false;
+            faceLeft = false;
             faceRight = true;
         }
     }
